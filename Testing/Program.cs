9f20f522f63b5ccfeab32893b373d84fa68f79cf
Bishoy Sedra <bishosedra0@gmail.com>
2024-03-04 12:06:26 +0200

    Testing Application Created!

---
+++ Testing/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static string Encrypt(string plainText, string key)
+    {
+        return "";
+        //throw new NotImplementedException();
+    }
+
+    static string Decrypt(string cipherText, string key)
+    {
+        return "";
+        //throw new NotImplementedException();
+    }
+
+    static void Main(string[] args)
+    {
+        do
+        {
+            Console.WriteLine("Choose the operation: \n encryption -> type 'e' \n decryption -> type 'd' ");
+            string op = Console.ReadLine().ToLower();
+
+            if (op == "e")
+            {
+                Console.WriteLine("PlainText: ");
+                string plainText = Console.ReadLine();
+
+                Console.WriteLine("Key: ");
+                string k = Console.ReadLine();
+
+                Console.WriteLine("Encrypted Text: ");
+                Console.WriteLine(Encrypt(plainText, k));
+
+                Console.WriteLine("=========================");
+
+                continue;
+            }
+
+            Console.WriteLine("cipherText: ");
+            string cipherText = Console.ReadLine();
+
+            Console.WriteLine("Key: ");
+            string key = Console.ReadLine();
+
+            //Console.WriteLine("Decrypted Text: ");
+            Console.WriteLine(Decrypt(cipherText, key));
+
+            Console.WriteLine("=========================");
+
+        } while (true);
+    }
+}
---
+++ Testing/README.md
@@ -0,0 +1,17 @@
+# Testing Application
+
+The Testing Application is designed to verify the functionality of encryption and decryption algorithms by testing them with various inputs.
+
+## Overview
+
+This application is specifically crafted to ensure that the encryption and decryption algorithms implemented in your project perform as expected under different scenarios and input conditions.
+
+## Getting Started
+
+Follow these simple steps to set up and run the application:
+
+1. Fill in the `Encrypt` function and `Decrypt` function with your implementations of the encryption and decryption algorithms, respectively.
+
+2. Run the application by pressing `CTRL + F5` or by using your preferred method to compile and execute the code.
+
+Ensure that your implementations are correct and handle all possible inputs effectively.

56296f7aa78535a264f2784beb6e58191c668e62
Momen Kadry <kadry.momen6@proton.me>
2024-03-05 21:33:43 +0200

    Decryption Done

--- startupcode/securitylibrary/MainAlgorithms/HillCipher.cs
+++ startupcode/securitylibrary/MainAlgorithms/HillCipher.cs
@@ -32,6 +32,13 @@ namespace SecurityLibrary
             List<List<int>> matrixAugmented = new List<List<int>>();
             List<List<int>> keyMatrix = new List<List<int>>();
             List<List<int>> inverseMatrix = new List<List<int>>();
+            List<List<int>> plain = new List<List<int>>();
+            List<int> plainArr = new List<int>();
+            int bValue = -1;
+            double bRemainder;
+            int mod = 1;
+            int cipherSize = (int)Math.Sqrt(keySize);
+
 
             // Create array holding letters
             Dictionary<char, int> letters = new Dictionary<char, int>();
@@ -72,6 +79,8 @@ namespace SecurityLibrary
             int index = 0;
             int columns = (int)Math.Ceiling((double)cipherText.Count / matrixSize);
 
+            Console.WriteLine("Col: " + columns);
+
             for (int i = 0; i < columns; i++)
             {
                 List<int> column = new List<int>();
@@ -89,8 +98,13 @@ namespace SecurityLibrary
             int keyMatrixCount = keyMatrix.Count;
             int keyMatrixColumns = keyMatrix[0].Count;
 
-            int cipherMatrixCount = cipherTextMatrix.Count;
-            int cipherMatrixColumns = cipherTextMatrix[0].Count;
+            int cipherMatrixCount = cipherTextMatrix.Count; // kam row
+            int cipherMatrixColumns = cipherTextMatrix[0].Count; // kol row feeh kam
+
+            Console.WriteLine(keyMatrixCount);
+            Console.WriteLine(keyMatrixColumns);
+            Console.WriteLine(cipherMatrixCount);
+            Console.WriteLine(cipherMatrixColumns);
 
             //// Inverse matrix
 
@@ -98,81 +112,285 @@ namespace SecurityLibrary
             switch (matrixSize)
             {
                 case 2: // Case: 2x2 matrix
-                    int determinant2 = 1 / ((keyMatrix[0][0] * keyMatrix[1][1]) - (keyMatrix[0][1] * keyMatrix[1][0]));
+                    int determinant2 = ((keyMatrix[0][0] * keyMatrix[1][1]) - (keyMatrix[0][1] * keyMatrix[1][0])) % 26;
+
+                    while (determinant2 < 0)
+                    {
+                        determinant2 += 26;
+                        determinant2 %= 26;
+                    }
+
+                    // Check GCD between 26 & determinant
+
+                    int tempDeterminant2 = determinant2;
+                    int GCDValue = 26;
+                    while (tempDeterminant2 != 0)
+                    {
+                        int rem = GCDValue % tempDeterminant2;
+                        GCDValue = tempDeterminant2;
+                        tempDeterminant2 = rem;
+                    }
+
+                    if (GCDValue != 1)
+                        throw new SystemException();
+
+                    while (bValue < 0) // loop until we get a viable `b` value (+ve int)
+                    {
+                        int determinantRemainder = 26 - determinant2;
+                        //Console.WriteLine("Det rem: " + determinantRemainder);
+                        //Console.WriteLine("mod b4: " + mod);
+                        bRemainder = (double)mod / (double)determinantRemainder;
+
+                        //Console.WriteLine("B rem: " + bRemainder);
+
+                        if ((bRemainder > 0 && bRemainder < 26) && (bRemainder == Math.Floor(bRemainder)))
+                            bValue = 26 - (int)bRemainder;
+                        else mod += 26;
+                        //Console.WriteLine("mod after: " + mod);
+                        //Console.WriteLine("B after: " + bValue);
+                    }
+
+                    Console.WriteLine("B: " + bValue);
+
+                    Console.WriteLine("Det2: " + determinant2);
                     int index11 = keyMatrix[1][1];
-                    int index10 = -1 * keyMatrix[1][0];
-                    int index01 = -1 * keyMatrix[0][1];
+                    int index10 = -1 * keyMatrix[1][0] % 26;
+                    int index01 = -1 * keyMatrix[0][1] % 26;
                     int index00 = keyMatrix[0][0];
+
+                    // Make sure no -ve values are present
+                    while (index10 < 0)
+                    {
+                        index10 += 26;
+                        index10 %= 26;
+                    }
+                    while (index01 < 0)
+                    {
+                        index01 += 26;
+                        index01 %= 26;
+                    }
+
+
                     List<int> row1 = new List<int>() { index11, index10 };
                     List<int> row2 = new List<int>() { index01, index00 };
                     List<List<int>> tempMatrix = new List<List<int>>();
+                    inverseMatrix.Add(new List<int> { 0, 0 });
+                    inverseMatrix.Add(new List<int> { 0, 0 });
                     tempMatrix.Add(row1);
                     tempMatrix.Add(row2);
-                    
+
+
+
                     for (int i = 0; i < matrixSize; i++)
                     {
-                        for (int j = 0;j < matrixSize; j++)
+                        for (int j = 0; j < matrixSize; j++)
                         {
-                            inverseMatrix[i][j] = determinant2 * tempMatrix[i][j];
+                            tempMatrix[i][j] *= bValue;
                         }
                     }
+
+                    for (int i = 0; i < matrixSize; i++)
+                    {
+                        for (int j = 0; j < matrixSize; j++)
+                        {
+                            inverseMatrix[i][j] = tempMatrix[j][i] % 26;
+                        }
+                        Console.WriteLine();
+                    }
+
+                    foreach (var row in inverseMatrix)
+                    {
+                        foreach (var c in row)
+                        {
+                            Console.Write(c + " ");
+                        }
+                        Console.WriteLine();
+                    }
+
 
                     break;
                 case 3: // Case: 3x3 matrix
                     int determinant3 = 0;
-                    int determinantRemainder;
-                    int bValue = -1;
-                    int bRemainder;
-                    int mod = 1;
+                    /*Console.WriteLine("----");
+                    foreach (var row in keyMatrix)
+                    {
+                        foreach (var num in row)
+                        {
+                            Console.Write(num + " ");
+                        }
+                        Console.WriteLine();
+                    }*/
 
-                    for (int i = 0;i < matrixSize; i++) // get determinant and modulo it
-                        determinant3 = determinant3 + (keyMatrix[0][i] * (keyMatrix[1][(i + 1) % 3] * keyMatrix[2][(i + 2) % 3] - keyMatrix[1][(i + 2) % 3] * keyMatrix[2][(i + 1) % 3]));
+                    Console.WriteLine("----");
+                    for (int i = 0; i < matrixSize; i++) // get determinant and modulo it
+                    {
+                        int subDeterminant = (keyMatrix[0][i] * (keyMatrix[1][(i + 1) % matrixSize] * keyMatrix[2][(i + 2) % matrixSize] -
+                                                                 keyMatrix[1][(i + 2) % matrixSize] * keyMatrix[2][(i + 1) % matrixSize]));
+                        determinant3 += subDeterminant;
+
+                        /*Console.WriteLine("0, " + (i + keyMatrix[0][i]) + "\t" + keyMatrix[0][i]);
+                        Console.WriteLine("1, " + (i + 1) % matrixSize + "\t" + keyMatrix[1][(i + 1) % matrixSize]);
+                        Console.WriteLine("2, " + (i + 2) % matrixSize + "\t" + keyMatrix[2][(i + 2) % matrixSize]);
+                        Console.WriteLine("1, " + (i + 2) % matrixSize + "\t" + keyMatrix[1][(i + 2) % matrixSize]);
+                        Console.WriteLine("2, " + (i + 1) % matrixSize + "\t" + keyMatrix[2][(i + 1) % matrixSize]);
+                        Console.WriteLine(determinant3);
+                        Console.WriteLine();*/
+
+
+                    }
                     determinant3 %= 26;
-                    
-                    while(bValue < 0) // loop until we get a viable `b` value (+ve int)
-                    {determinantRemainder = 26 - determinant3;
-                    bRemainder = mod / determinantRemainder;
 
-                        if (bRemainder > 0)
-                            bValue = 26 - bRemainder;
-                        else mod += 26;
+                    while (determinant3 < 0)
+                    {
+                        determinant3 += 26;
+                        determinant3 %= 26;
                     }
 
+                    // Check GCD between 26 & determinant
+
+                    int tempDeterminant3 = determinant3;
+                    int otherGCDValue = 26;
+                    while (tempDeterminant3 != 0)
+                    {
+                        int rem = otherGCDValue % tempDeterminant3;
+                        otherGCDValue = tempDeterminant3;
+                        tempDeterminant3 = rem;
+                    }
+
+                    if (otherGCDValue != 1)
+                        throw new SystemException();
+
+                    while (bValue < 0) // loop until we get a viable `b` value (+ve int)
+                    {
+                        int determinantRemainder = 26 - determinant3;
+
+                        //Console.WriteLine("mod b4: " + mod);
+                        bRemainder = (double)mod / (double)determinantRemainder;
+
+                        //Console.WriteLine("B rem: " + bRemainder);
+
+                        if ((bRemainder > 0 && bRemainder < 26) && (bRemainder == Math.Floor(bRemainder)))
+                            bValue = 26 - (int)bRemainder;
+                        else mod += 26;
+                        //Console.WriteLine("mod after: " + mod);
+                    }
+                    /*Console.WriteLine("B: " + bValue); // nova*/
+
+                    List<List<int>> adjointMatrix = new List<List<int>>();
                     for (int i = 0; i < matrixSize; i++)
                     {
+                        List<int> cofactorRow = new List<int>();
                         for (int j = 0; j < matrixSize; j++)
                         {
                             List<int> subDeterminantRow1 = new List<int>() { keyMatrix[(i + 1) % matrixSize][(j + 1) % matrixSize], keyMatrix[(i + 1) % matrixSize][(j + 2) % matrixSize] };
                             List<int> subDeterminantRow2 = new List<int>() { keyMatrix[(i + 2) % matrixSize][(j + 1) % matrixSize], keyMatrix[(i + 2) % matrixSize][(j + 2) % matrixSize] };
 
                             int subDeterminant = (subDeterminantRow1[0] * subDeterminantRow2[1]) - (subDeterminantRow1[1] * subDeterminantRow2[0]);
+
+                            while (subDeterminant < 0)
+                            {
+                                subDeterminant += 26;
+                                subDeterminant %= 26;
+                            }
                             subDeterminant %= 26;
-                            int one = (int)Math.Pow(-1, i + j);
-                            inverseMatrix[i][j] = (bValue * one * (subDeterminant)) % 26; // inverse of key matrix but not fr fr
+                            cofactorRow.Add(subDeterminant);
                         }
+                        adjointMatrix.Add(cofactorRow);
                     }
 
-                    List<List<int>> inverseMatrixFrFr = new List<List<int>>();
+                    /*foreach (var row in adjointMatrix)
+                    {
+                        foreach (var num in row)
+                        {
+                            Console.Write(num + " ");
+                        }
+                        Console.WriteLine();
+                    }*/
+
+
                     for (int i = 0; i < matrixSize; i++)
                     {
-                        for(int j = 0; j < matrixSize; j++)
-                        {
-                            inverseMatrixFrFr[i][j] = inverseMatrix[j][i]; // inverse of 3x3 matrix 
-                        }
-                    }
-
-                    break;
-                    /*List<List<int>> cofactorMatrix = new List<List<int>>(); // get inverse matrix fr fr
-                    for (int i = 0; i < matrixSize; i++)
+                        List<int> inverseRow = new List<int>();
                         for (int j = 0; j < matrixSize; j++)
                         {
-                            int cofactoredValues = (keyMatrix[(j + 1) % matrixSize][(i + 1) % matrixSize] * keyMatrix[(j + 2) % matrixSize][(i + 2) % matrixSize]) - (keyMatrix[(j + 1) % matrixSize][(i + 2) % matrixSize] * keyMatrix[(j + 2) % matrixSize][(i + 1) % matrixSize]);
-                            cofactorMatrix[i][j] = cofactoredValues / determinant3;
-                        }*/
+                            int one = (int)Math.Pow(-1, i + j);
+
+                            inverseRow.Add((bValue * (adjointMatrix[j][i])) % 26); // inverse of key matrix fr fr
+                        }
+                        inverseMatrix.Add(inverseRow);
+                    }
+
+
+                    /*foreach (var row in inverseMatrix)
+                    {
+                        foreach (var num in row)
+                        {
+                            Console.Write(num + " ");
+                        }
+                        Console.WriteLine();
+                    }*/
+
+                    break;
+            }
+            /*Console.WriteLine("Key Matrix: ");
+            foreach (var row in keyMatrix)
+            {
+                foreach (var num in row)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Cipher Matrix");
+            foreach (var row in cipherTextMatrix)
+            {
+                foreach (var num in row)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("------");*/
+
+
+            // matrix multiplication
+            for (int i = 0; i < keyMatrixCount; i++)
+            {
+                List<int> row = new List<int>();
+                for (int j = 0; j < cipherMatrixCount; j++)
+                {
+                    int sum = 0;
+                    for (int k = 0; k < keyMatrixColumns; k++)
+                    {
+                        //Console.WriteLine("inverse: " + inverseMatrix[i][k] + "\tcipher: " + cipherTextMatrix[j][k]);
+                        sum += inverseMatrix[i][k] * cipherTextMatrix[j][k];
+
+                        sum %= 26;
+                    }
+                    row.Add(sum);
+                }
+                plain.Add(row);
+            }
+
+            int plainCount = plain.Count;
+            int plainColumns = plain[0].Count;
+
+            // convert cipher numbers to letters
+            for (int i = 0; i < plainColumns; i++)
+            {
+                for (int j = 0; j < plainCount; j++)
+                {
+                    plainArr.Add(plain[j][i]);
+                }
             }
 
 
-            throw new NotImplementedException();
+            return plainArr;
+            
+            //throw new NotImplementedException();
         }
 
         public string Decrypt(string cipherText, string key)
@@ -181,6 +399,7 @@ namespace SecurityLibrary
         }
 
         public List<int> Encrypt(List<int> plainText, List<int> key)
+
         {
             // Variables
             int keySize = key.Count;

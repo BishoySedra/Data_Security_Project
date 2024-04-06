2cd31f2a3d362c88b19fc131ae592b40b3fb9939
mo-ibrahim22 <mibrahimm002@gmail.com>
2024-04-06 07:29:09 +0200

    AES done

--- AES.cs
+++ AES.cs
@@ -13,7 +13,76 @@ namespace SecurityLibrary.AES
     {
         public override string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+
+            // make sure that the cipher text and key don't start with "0x"
+            if (cipherText.StartsWith("0x"))
+            {
+                cipherText = cipherText.Substring(2);
+            }
+
+            if (key.StartsWith("0x"))
+            {
+                key = key.Substring(2);
+            }
+
+            // make cipher text and key upper case
+            cipherText = cipherText.ToUpper();
+            key = key.ToUpper();
+
+            // generate state matrix with the cipher text and key
+            string[,] cipherState = generateStateMatrix(cipherText);
+            string[,] keyState = generateStateMatrix(key);
+
+            // generate round keys in reverse order
+            for (int i = 0; i <= 9; i++)
+            {
+                keyState = GenerateRoundKey(keyState, i);
+            }
+
+            // add initial round key
+            cipherState = addRoundKey(cipherState, keyState);
+
+            // perform rounds in reverse order
+            for (int i = 9; i > 0; i--)
+            {
+                // reverse operations of encryption rounds
+                cipherState = shiftRows(cipherState, true);
+                cipherState = subBytes(cipherState, true);
+
+                // regenerate round key for each round
+                keyState = generateStateMatrix(key);
+                for (int j = 0; j < i; j++)
+                {
+                    keyState = GenerateRoundKey(keyState, j);
+                }
+
+                // add round key
+                cipherState = addRoundKey(keyState, cipherState);
+
+                // reverse mix columns operation
+                cipherState = mixColumns(cipherState, true);
+            }
+
+            // final round operations (without mix columns)
+            cipherState = shiftRows(cipherState, true);
+            cipherState = subBytes(cipherState, true);
+            keyState = generateStateMatrix(key);
+            cipherState = addRoundKey(keyState, cipherState);
+
+            // convert the state matrix to result
+            string result = "0x";
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    result += cipherState[j, i];
+                }
+            }
+
+            return result;
+
+            //throw new NotImplementedException();
         }
 
         public override string Encrypt(string plainText, string key)
@@ -43,13 +112,13 @@ namespace SecurityLibrary.AES
             for (int i = 0; i < 9; i++)
             {
                 // (2) sub bytes
-                plainState = subBytes(plainState);
+                plainState = subBytes(plainState, false);
 
                 // (3) shift rows
-                plainState = shiftRows(plainState);
+                plainState = shiftRows(plainState, false);
 
                 // (4) mix columns
-                plainState = mixColumns(plainState);
+                plainState = mixColumns(plainState, false);
 
                 // (5) generate round key
                 keyState = GenerateRoundKey(keyState, i);
@@ -59,10 +128,10 @@ namespace SecurityLibrary.AES
             }
 
             // (7) sub bytes
-            plainState = subBytes(plainState);
+            plainState = subBytes(plainState, false);
 
             // (8) shift rows
-            plainState = shiftRows(plainState);
+            plainState = shiftRows(plainState, false);
 
             // (9) generate round key
             keyState = GenerateRoundKey(keyState, 9);
@@ -227,7 +296,7 @@ namespace SecurityLibrary.AES
             return string.Empty;
         }
 
-        string[,] mixColumns(string[,] plain)
+        string[,] mixColumns(string[,] text, bool inverse)
         {
             string[,] result = new string[4, 4];
             string[,] mixColumnMatrix = new string[4, 4]
@@ -238,13 +307,25 @@ namespace SecurityLibrary.AES
         {"03", "01", "01", "02"}
             };
 
+            if (inverse == true)
+            {
+                // Use inverse mix column matrix
+                mixColumnMatrix = new string[4, 4]
+                {
+            {"0E", "0B", "0D", "09"},
+            {"09", "0E", "0B", "0D"},
+            {"0D", "09", "0E", "0B"},
+            {"0B", "0D", "09", "0E"}
+                };
+            }
+
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
                     for (int k = 0; k < 4; k++)
                     {
-                        string binary = convertToBinary(plain[k, j]);
+                        string binary = convertToBinary(text[k, j]);
                         string hex = mixColumnMatrix[i, k];
                         if (result[i, j] == null)
                         {
@@ -263,68 +344,100 @@ namespace SecurityLibrary.AES
             return result;
         }
 
-        string[,] shiftRows(string[,] plain)
+        string[,] shiftRows(string[,] text, bool inverse)
         {
-
             string[,] result = new string[4, 4];
 
-            for (int i = 0; i < 4; i++)
+            if (inverse == true)
             {
-                for (int j = 0; j < 4; j++)
+                // Perform inverse shift rows
+                for (int i = 0; i < 4; i++)
                 {
-                    result[i, j] = plain[i, (j + i) % 4];
+                    for (int j = 0; j < 4; j++)
+                    {
+                        result[i, j] = text[i, (j - i + 4) % 4];
+                    }
                 }
             }
-            return result;
+            else
+            {
+                // Perform normal shift rows
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        result[i, j] = text[i, (j + i) % 4];
+                    }
+                }
+            }
 
+            return result;
         }
 
-        string[,] subBytes(string[,] plain)
+
+        string[,] subBytes(string[,] text, bool inverse)
         {
             Dictionary<string, string> sBox = new Dictionary<string, string>()
-            {
-                {"00", "63"}, {"01", "7C"}, {"02", "77"}, {"03", "7B"}, {"04", "F2"}, {"05", "6B"}, {"06", "6F"}, {"07", "C5"},
-                {"08", "30"}, {"09", "01"}, {"0A", "67"}, {"0B", "2B"}, {"0C", "FE"}, {"0D", "D7"}, {"0E", "AB"}, {"0F", "76"},
-                {"10", "CA"}, {"11", "82"}, {"12", "C9"}, {"13", "7D"}, {"14", "FA"}, {"15", "59"}, {"16", "47"}, {"17", "F0"},
-                {"18", "AD"}, {"19", "D4"}, {"1A", "A2"}, {"1B", "AF"}, {"1C", "9C"}, {"1D", "A4"}, {"1E", "72"}, {"1F", "C0"},
-                {"20", "B7"}, {"21", "FD"}, {"22", "93"}, {"23", "26"}, {"24", "36"}, {"25", "3F"}, {"26", "F7"}, {"27", "CC"},
-                {"28", "34"}, {"29", "A5"}, {"2A", "E5"}, {"2B", "F1"}, {"2C", "71"}, {"2D", "D8"}, {"2E", "31"}, {"2F", "15"},
-                {"30", "04"}, {"31", "C7"}, {"32", "23"}, {"33", "C3"}, {"34", "18"}, {"35", "96"}, {"36", "05"}, {"37", "9A"},
-                {"38", "07"}, {"39", "12"}, {"3A", "80"}, {"3B", "E2"}, {"3C", "EB"}, {"3D", "27"}, {"3E", "B2"}, {"3F", "75"},
-                {"40", "09"}, {"41", "83"}, {"42", "2C"}, {"43", "1A"}, {"44", "1B"}, {"45", "6E"}, {"46", "5A"}, {"47", "A0"},
-                {"48", "52"}, {"49", "3B"}, {"4A", "D6"}, {"4B", "B3"}, {"4C", "29"}, {"4D", "E3"}, {"4E", "2F"}, {"4F", "84"},
-                {"50", "53"}, {"51", "D1"}, {"52", "00"}, {"53", "ED"}, {"54", "20"}, {"55", "FC"}, {"56", "B1"}, {"57", "5B"},
-                {"58", "6A"}, {"59", "CB"}, {"5A", "BE"}, {"5B", "39"}, {"5C", "4A"}, {"5D", "4C"}, {"5E", "58"}, {"5F", "CF"},
-                {"60", "D0"}, {"61", "EF"}, {"62", "AA"}, {"63", "FB"}, {"64", "43"}, {"65", "4D"}, {"66", "33"}, {"67", "85"},
-                {"68", "45"}, {"69", "F9"}, {"6A", "02"}, {"6B", "7F"}, {"6C", "50"}, {"6D", "3C"}, {"6E", "9F"}, {"6F", "A8"},
-                {"70", "51"}, {"71", "A3"}, {"72", "40"}, {"73", "8F"}, {"74", "92"}, {"75", "9D"}, {"76", "38"}, {"77", "F5"},
-                {"78", "BC"}, {"79", "B6"}, {"7A", "DA"}, {"7B", "21"}, {"7C", "10"}, {"7D", "FF"}, {"7E", "F3"}, {"7F", "D2"},
-                {"80", "CD"}, {"81", "0C"}, {"82", "13"}, {"83", "EC"}, {"84", "5F"}, {"85", "97"}, {"86", "44"}, {"87", "17"},
-                {"88", "C4"}, {"89", "A7"}, {"8A", "7E"}, {"8B", "3D"}, {"8C", "64"}, {"8D", "5D"}, {"8E", "19"}, {"8F", "73"},
-                {"90", "60"}, {"91", "81"}, {"92", "4F"}, {"93", "DC"}, {"94", "22"}, {"95", "2A"}, {"96", "90"}, {"97", "88"},
-                {"98", "46"}, {"99", "EE"}, {"9A", "B8"}, {"9B", "14"}, {"9C", "DE"}, {"9D", "5E"}, {"9E", "0B"}, {"9F", "DB"},
-                {"A0", "E0"}, {"A1", "32"}, {"A2", "3A"}, {"A3", "0A"}, {"A4", "49"}, {"A5", "06"}, {"A6", "24"}, {"A7", "5C"},
-                {"A8", "C2"}, {"A9", "D3"}, {"AA", "AC"}, {"AB", "62"}, {"AC", "91"}, {"AD", "95"}, {"AE", "E4"}, {"AF", "79"},
-                {"B0", "E7"}, {"B1", "C8"}, {"B2", "37"}, {"B3", "6D"}, {"B4", "8D"}, {"B5", "D5"}, {"B6", "4E"}, {"B7", "A9"},
-                {"B8", "6C"}, {"B9", "56"}, {"BA", "F4"}, {"BB", "EA"}, {"BC", "65"}, {"BD", "7A"}, {"BE", "AE"}, {"BF", "08"},
-                {"C0", "BA"}, {"C1", "78"}, {"C2", "25"}, {"C3", "2E"}, {"C4", "1C"}, {"C5", "A6"}, {"C6", "B4"}, {"C7", "C6"},
-                {"C8", "E8"}, {"C9", "DD"}, {"CA", "74"}, {"CB", "1F"}, {"CC", "4B"}, {"CD", "BD"}, {"CE", "8B"}, {"CF", "8A"},
-                {"D0", "70"}, {"D1", "3E"}, {"D2", "B5"}, {"D3", "66"}, {"D4", "48"}, {"D5", "03"}, {"D6", "F6"}, {"D7", "0E"},
-                {"D8", "61"}, {"D9", "35"}, {"DA", "57"}, {"DB", "B9"}, {"DC", "86"}, {"DD", "C1"}, {"DE", "1D"}, {"DF", "9E"},
-                {"E0", "E1"}, {"E1", "F8"}, {"E2", "98"}, {"E3", "11"}, {"E4", "69"}, {"E5", "D9"}, {"E6", "8E"}, {"E7", "94"},
-                {"E8", "9B"}, {"E9", "1E"}, {"EA", "87"}, {"EB", "E9"}, {"EC", "CE"}, {"ED", "55"}, {"EE", "28"}, {"EF", "DF"},
-                {"F0", "8C"}, {"F1", "A1"}, {"F2", "89"}, {"F3", "0D"}, {"F4", "BF"}, {"F5", "E6"}, {"F6", "42"}, {"F7", "68"},
-                {"F8", "41"}, {"F9", "99"}, {"FA", "2D"}, {"FB", "0F"}, {"FC", "B0"}, {"FD", "54"}, {"FE", "BB"}, {"FF", "16"} };
+    {
+        {"00", "63"}, {"01", "7C"}, {"02", "77"}, {"03", "7B"}, {"04", "F2"}, {"05", "6B"}, {"06", "6F"}, {"07", "C5"},
+        {"08", "30"}, {"09", "01"}, {"0A", "67"}, {"0B", "2B"}, {"0C", "FE"}, {"0D", "D7"}, {"0E", "AB"}, {"0F", "76"},
+        {"10", "CA"}, {"11", "82"}, {"12", "C9"}, {"13", "7D"}, {"14", "FA"}, {"15", "59"}, {"16", "47"}, {"17", "F0"},
+        {"18", "AD"}, {"19", "D4"}, {"1A", "A2"}, {"1B", "AF"}, {"1C", "9C"}, {"1D", "A4"}, {"1E", "72"}, {"1F", "C0"},
+        {"20", "B7"}, {"21", "FD"}, {"22", "93"}, {"23", "26"}, {"24", "36"}, {"25", "3F"}, {"26", "F7"}, {"27", "CC"},
+        {"28", "34"}, {"29", "A5"}, {"2A", "E5"}, {"2B", "F1"}, {"2C", "71"}, {"2D", "D8"}, {"2E", "31"}, {"2F", "15"},
+        {"30", "04"}, {"31", "C7"}, {"32", "23"}, {"33", "C3"}, {"34", "18"}, {"35", "96"}, {"36", "05"}, {"37", "9A"},
+        {"38", "07"}, {"39", "12"}, {"3A", "80"}, {"3B", "E2"}, {"3C", "EB"}, {"3D", "27"}, {"3E", "B2"}, {"3F", "75"},
+        {"40", "09"}, {"41", "83"}, {"42", "2C"}, {"43", "1A"}, {"44", "1B"}, {"45", "6E"}, {"46", "5A"}, {"47", "A0"},
+        {"48", "52"}, {"49", "3B"}, {"4A", "D6"}, {"4B", "B3"}, {"4C", "29"}, {"4D", "E3"}, {"4E", "2F"}, {"4F", "84"},
+        {"50", "53"}, {"51", "D1"}, {"52", "00"}, {"53", "ED"}, {"54", "20"}, {"55", "FC"}, {"56", "B1"}, {"57", "5B"},
+        {"58", "6A"}, {"59", "CB"}, {"5A", "BE"}, {"5B", "39"}, {"5C", "4A"}, {"5D", "4C"}, {"5E", "58"}, {"5F", "CF"},
+        {"60", "D0"}, {"61", "EF"}, {"62", "AA"}, {"63", "FB"}, {"64", "43"}, {"65", "4D"}, {"66", "33"}, {"67", "85"},
+        {"68", "45"}, {"69", "F9"}, {"6A", "02"}, {"6B", "7F"}, {"6C", "50"}, {"6D", "3C"}, {"6E", "9F"}, {"6F", "A8"},
+        {"70", "51"}, {"71", "A3"}, {"72", "40"}, {"73", "8F"}, {"74", "92"}, {"75", "9D"}, {"76", "38"}, {"77", "F5"},
+        {"78", "BC"}, {"79", "B6"}, {"7A", "DA"}, {"7B", "21"}, {"7C", "10"}, {"7D", "FF"}, {"7E", "F3"}, {"7F", "D2"},
+        {"80", "CD"}, {"81", "0C"}, {"82", "13"}, {"83", "EC"}, {"84", "5F"}, {"85", "97"}, {"86", "44"}, {"87", "17"},
+        {"88", "C4"}, {"89", "A7"}, {"8A", "7E"}, {"8B", "3D"}, {"8C", "64"}, {"8D", "5D"}, {"8E", "19"}, {"8F", "73"},
+        {"90", "60"}, {"91", "81"}, {"92", "4F"}, {"93", "DC"}, {"94", "22"}, {"95", "2A"}, {"96", "90"}, {"97", "88"},
+        {"98", "46"}, {"99", "EE"}, {"9A", "B8"}, {"9B", "14"}, {"9C", "DE"}, {"9D", "5E"}, {"9E", "0B"}, {"9F", "DB"},
+        {"A0", "E0"}, {"A1", "32"}, {"A2", "3A"}, {"A3", "0A"}, {"A4", "49"}, {"A5", "06"}, {"A6", "24"}, {"A7", "5C"},
+        {"A8", "C2"}, {"A9", "D3"}, {"AA", "AC"}, {"AB", "62"}, {"AC", "91"}, {"AD", "95"}, {"AE", "E4"}, {"AF", "79"},
+        {"B0", "E7"}, {"B1", "C8"}, {"B2", "37"}, {"B3", "6D"}, {"B4", "8D"}, {"B5", "D5"}, {"B6", "4E"}, {"B7", "A9"},
+        {"B8", "6C"}, {"B9", "56"}, {"BA", "F4"}, {"BB", "EA"}, {"BC", "65"}, {"BD", "7A"}, {"BE", "AE"}, {"BF", "08"},
+        {"C0", "BA"}, {"C1", "78"}, {"C2", "25"}, {"C3", "2E"}, {"C4", "1C"}, {"C5", "A6"}, {"C6", "B4"}, {"C7", "C6"},
+        {"C8", "E8"}, {"C9", "DD"}, {"CA", "74"}, {"CB", "1F"}, {"CC", "4B"}, {"CD", "BD"}, {"CE", "8B"}, {"CF", "8A"},
+        {"D0", "70"}, {"D1", "3E"}, {"D2", "B5"}, {"D3", "66"}, {"D4", "48"}, {"D5", "03"}, {"D6", "F6"}, {"D7", "0E"},
+        {"D8", "61"}, {"D9", "35"}, {"DA", "57"}, {"DB", "B9"}, {"DC", "86"}, {"DD", "C1"}, {"DE", "1D"}, {"DF", "9E"},
+        {"E0", "E1"}, {"E1", "F8"}, {"E2", "98"}, {"E3", "11"}, {"E4", "69"}, {"E5", "D9"}, {"E6", "8E"}, {"E7", "94"},
+        {"E8", "9B"}, {"E9", "1E"}, {"EA", "87"}, {"EB", "E9"}, {"EC", "CE"}, {"ED", "55"}, {"EE", "28"}, {"EF", "DF"},
+        {"F0", "8C"}, {"F1", "A1"}, {"F2", "89"}, {"F3", "0D"}, {"F4", "BF"}, {"F5", "E6"}, {"F6", "42"}, {"F7", "68"},
+        {"F8", "41"}, {"F9", "99"}, {"FA", "2D"}, {"FB", "0F"}, {"FC", "B0"}, {"FD", "54"}, {"FE", "BB"}, {"FF", "16"}
+    };
 
-            for (int i = 0; i < 4; i++)
+            if (inverse == true)
             {
-                for (int j = 0; j < 4; j++)
+                // Perform inverse operation
+                Dictionary<string, string> inverseSBox = sBox.ToDictionary(kv => kv.Value, kv => kv.Key);
+                for (int i = 0; i < 4; i++)
                 {
-                    plain[j, i] = sBox[plain[j, i]];
+                    for (int j = 0; j < 4; j++)
+                    {
+                        text[j, i] = inverseSBox[text[j, i]];
+                    }
+                }
+            }
+            else
+            {
+                // Perform normal operation
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        text[j, i] = sBox[text[j, i]];
+                    }
                 }
             }
 
-            return plain;
+            return text;
         }
 
         string[,] generateStateMatrix(string text)
@@ -486,14 +599,14 @@ namespace SecurityLibrary.AES
             return result;
         }
 
-        string[,] addRoundKey(string[,] plain, string[,] key)
+        string[,] addRoundKey(string[,] text, string[,] key)
         {
             string[,] result = new string[4, 4];
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
-                    result[j, i] = XOR(convertToBinary(plain[j, i]), convertToBinary(key[j, i]));
+                    result[j, i] = XOR(convertToBinary(text[j, i]), convertToBinary(key[j, i]));
                 }
             }
 

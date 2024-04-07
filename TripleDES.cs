c848c17120e561a5baebca06ccac58951b8bd010
Bishoy Sedra <bishosedra0@gmail.com>
2024-04-07 17:31:14 +0200

    Triple DES Done!

--- TripleDES.cs
+++ TripleDES.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace SecurityLibrary.DES
 {
     /// <summary>
@@ -13,15 +12,23 @@ namespace SecurityLibrary.DES
     {
         public string Decrypt(string cipherText, List<string> key)
         {
-            throw new NotImplementedException();
+            DES des = new DES();
+            string plain = des.Decrypt(cipherText, key[0]);
+            plain = des.Encrypt(plain, key[1]);
+            plain = des.Decrypt(plain, key[0]);
+            return plain;
         }
 
         public string Encrypt(string plainText, List<string> key)
         {
-            throw new NotImplementedException();
+            DES des = new DES();
+            string cipher = des.Encrypt(plainText, key[0]);
+            cipher = des.Decrypt(cipher, key[1]);
+            cipher = des.Encrypt(cipher, key[0]);
+            return cipher;
         }
 
-        public List<string> Analyse(string plainText,string cipherText)
+        public List<string> Analyse(string plainText, string cipherText)
         {
             throw new NotSupportedException();
         }
